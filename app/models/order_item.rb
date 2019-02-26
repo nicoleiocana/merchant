ca91d7bf46d975930415c5cedfcfb6606b1c9c3f
Nicolei Ocana <nicoleiocana@gmail.com>
2019-02-26 17:33:52 +0000

    design orders

---
+++ app/models/order_item.rb
@@ -0,0 +1,5 @@
+class OrderItem < ApplicationRecord
+  belongs_to :order
+  belongs_to :product
+  validates :order_id, :product_id, presence: true
+end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ApplicationRecord
   validates :price, :title, :description, presence: true
   validates :price, numericality: true
   validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  has_many :order_items
   
   def price=(input)
     super(input.tr('$, ', ''))
