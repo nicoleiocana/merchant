efd2aed472b87cbff58359e78581eca4d227c474
Nicolei Ocana <nicoleiocana@gmail.com>
2019-02-26 16:08:04 +0000

    handle stock input

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,7 @@
 class Product < ApplicationRecord
   validates :price, :title, :description, presence: true
-  validates_numericality_of :price
+  validates :price, numericality: true
+  validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   
   def price=(input)
     super(input.tr('$, ', ''))
